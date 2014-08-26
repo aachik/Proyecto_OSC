efa91d08d725bc708bf0dc43fcf7ff4bf83794f5
aachik <abdulhamid@outlook.com>
2014-08-26 17:57:09 -0500

    cambie el diseño y anduve haciendo pruebas con el panel de administracion

---
+++ app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from app.models import Organizacion
+from app.models import Actividades
+from app.models import AreasInteres
+from app.models import RedSocial
+from app.models import TipoOrganizacion
+
+admin.site.register(Organizacion)
+admin.site.register(AreasInteres)
+admin.site.register(Actividades)
+admin.site.register(RedSocial)
+admin.site.register(TipoOrganizacion)
