8e5807b9eff71b9cd66e3e7d70a4748896c8ef06
aachik <abdulhamid@outlook.com>
2014-08-26 19:33:00 -0500

    agregue los archivos que no podia agregar

---
+++ Proyecto_OSC/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import patterns, include, url
+
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
+urlpatterns = patterns('',
+
+  url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+)
+
+
+urlpatterns = patterns('app.views',
+
+    (r'^admin/', include(admin.site.urls)),
+    (r"^organizacion/(?P<pk>\d+)/$", "organizacion"),
+    (r"^form/","form"),
+
+    (r"","main"),
+
+
+)
+
+if settings.DEBUG and settings.STATIC_ROOT:
+    urlpatterns += patterns('',
+        (r'%s(?P<path>.*)$' % settings.STATIC_URL.lstrip('/'),
+            'django.views.static.serve',
+            {'document_root' : settings.STATIC_ROOT }),)
