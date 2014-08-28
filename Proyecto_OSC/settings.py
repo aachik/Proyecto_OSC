b322fac304aa45f629d4519efa85f117757f5791
aachik <abdulhamid@outlook.com>
2014-08-28 17:16:31 -0500

    arregle la coneccion a postgres, agregue que se vieran las actividades, redes sociales, y areas de interes

--- Proyecto_OSC/settings.py
+++ Proyecto_OSC/settings.py
@@ -54,23 +54,24 @@ ROOT_URLCONF = 'Proyecto_OSC.urls'
 WSGI_APPLICATION = 'Proyecto_OSC.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'db.sqlite3',
+#    }
+#}
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   ##    'USER': 'postgresql',
-     #   'PASSWORD': 'aa121292',
-      #  'HOST': '',
-       # 'PORT': '5432'
-    #}
-#}
+DATABASES = {
+    'default': {
+       'NAME': 'Proyecto_OSC',
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'USER': 'postgres',
+       'PASSWORD': 'aa121292',
+       'HOST': '',
+       'PORT': '5432'
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
